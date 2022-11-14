723a1d35381b9066bd557f4078a677dd9bc635ec
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-14 19:16:34 +0400

    Добавлена анимация на модальное окно

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,4 +1,5 @@
 "use strict";
+import {animate} from "./helpers";
 const modal = () => {
     const modalCallback = document.getElementById('callback');
     const modalOverlay  = document.querySelector('.modal-overlay');
@@ -6,8 +7,19 @@ const modal = () => {
     const callbackBtn = document.querySelectorAll('.callback-btn');
 
     const modalMenu = () => {
+  
         callbackBtn.forEach(btn => {
         btn.addEventListener('click', () => {
+            animate({
+                duration: 400,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    modalCallback.style.left = (50 * progress) + '%';
+                    modalCallback.style.top = (25 * progress) + '%';
+                }
+            });
             console.log("Hello");
             modalOverlay.style.display = 'block';
             modalCallback.style.display = 'block';
