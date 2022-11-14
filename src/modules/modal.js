e7662ac8258062838c0020f3bcb04b1ac52babb3
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-14 19:19:03 +0400

    Добавлена анимация на модальное окно

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,32 +1,32 @@
 "use strict";
-import {animate} from "./helpers";
+import { animate } from "./helpers";
 const modal = () => {
     const modalCallback = document.getElementById('callback');
-    const modalOverlay  = document.querySelector('.modal-overlay');
+    const modalOverlay = document.querySelector('.modal-overlay');
     const modalClose = document.querySelector('.modal-close');
     const callbackBtn = document.querySelectorAll('.callback-btn');
 
     const modalMenu = () => {
-  
+
         callbackBtn.forEach(btn => {
-        btn.addEventListener('click', () => {
-            animate({
-                duration: 400,
-                timing(timeFraction) {
-                    return timeFraction;
-                },
-                draw(progress) {
-                    modalCallback.style.left = (50 * progress) + '%';
-                    modalCallback.style.top = (25 * progress) + '%';
-                }
+            btn.addEventListener('click', () => {
+                animate({
+                    duration: 400,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        modalCallback.style.left = (50 * progress) + '%';
+                        modalCallback.style.top = (25 * progress) + '%';
+                    }
+                });
+                modalOverlay.style.display = 'block';
+                modalCallback.style.display = 'block';
             });
-            modalOverlay.style.display = 'block';
-            modalCallback.style.display = 'block';
-        });
-        modalClose.addEventListener('click', (e) => {
-            modalOverlay.style.display = 'none';
-            modalCallback.style.display = 'none';
-        });
+            modalClose.addEventListener('click', (e) => {
+                modalOverlay.style.display = 'none';
+                modalCallback.style.display = 'none';
+            });
         });
     };
     modalMenu();
