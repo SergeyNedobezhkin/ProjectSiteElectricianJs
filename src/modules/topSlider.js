b989091170ca9b1d51dfd87ee878b3e3ebb18630
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-19 09:55:17 +0400

    Исправил главный слайдер

--- src/modules/topSlider.js
+++ src/modules/topSlider.js
@@ -1,29 +1,29 @@
 const topSlider = () => {
     const slides = document.querySelectorAll('.item');
-
     const tableText = document.querySelectorAll('.table');
     let currentSlide = 0;
 
-    tableText.forEach(item => {
-        item.classList.add("active");
-    });
+    tableText[currentSlide].classList.add("active");
 
     const prevSlide = () => {
         slides[currentSlide].style.display = "none";
-
+        tableText[currentSlide].classList.remove("active");
     };
+
     const nextSlide = () => {
         slides[currentSlide].style.display = "block";
-
+        tableText[currentSlide].classList.add("active");
     };
 
     const autoSlide = () => {
+
         prevSlide();
 
         currentSlide++;
         if (currentSlide >= slides.length) {
             currentSlide = 0;
         }
+
         nextSlide();
     };
 
