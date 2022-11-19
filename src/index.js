182b5c259c301c1b4f4e0d5bc6115fb4bcf4be2f
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-19 19:40:39 +0400

    Реализован блок sendForm

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import modalСall from './modules/modalСall';
-import topSlider from './modules/topSlider';
-import modalServices from './modules/modalServices';
-import modalSlades from './modules/swiperServices';
-import {swiperFunc} from './modules/swiperServices';
-import smoothScrolling from './modules/smoothScrolling';
-import faq from './modules/faq';
 import sendForm from './modules/sendForm';
 import check from './modules/check';
 modalСall();
-topSlider();
-modalServices();
-modalSlades();
-swiperFunc();
-smoothScrolling();
-faq();
 check();
 sendForm('form-callback');
-// sendForm('form-callback');
+
 
 
