ce69b745d1bc49154dd04f394a55ec6aa481740d
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-19 19:27:17 +0400

    Реализован блок swiperServices

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-import modalСall from './modules/modalСall';
-import topSlider from './modules/topSlider';
-import modalServices from './modules/modalServices';
-import modalSlades from './modules/swiperServices';
 import {swiperFunc} from './modules/swiperServices';
 
-modalСall();
-topSlider();
-modalServices();
-modalSlades();
 swiperFunc();
 
 
