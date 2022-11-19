fa66309ae046898dd10df51ff79b413deb94b890
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-19 19:34:56 +0400

    Реализован блок smoothScrolling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import modalСall from './modules/modalСall';
-import topSlider from './modules/topSlider';
-import modalServices from './modules/modalServices';
-import modalSlades from './modules/swiperServices';
-import {swiperFunc} from './modules/swiperServices';
 import smoothScrolling from './modules/smoothScrolling';
 
-modalСall();
-topSlider();
-modalServices();
-modalSlades();
-swiperFunc();
 smoothScrolling();
 
 
