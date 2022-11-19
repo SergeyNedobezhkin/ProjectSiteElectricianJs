c2487153877a6838d3e7c8e700490c294f4a7606
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-19 19:16:09 +0400

    Реализован блок FAQ

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import modalСall from './modules/modalСall';
-import topSlider from './modules/topSlider';
-import modalServices from './modules/modalServices';
-import modalSlades from './modules/swiperServices';
-import {swiperFunc} from './modules/swiperServices';
-import smoothScrolling from './modules/smoothScrolling';
+
 import faq from './modules/faq';
-modalСall();
-topSlider();
-modalServices();
-modalSlades();
-swiperFunc();
 faq();
-smoothScrolling();
+
 
 
